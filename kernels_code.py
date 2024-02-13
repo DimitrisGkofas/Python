17c84e6677b1adfc00898ed38098e66a80c22d10
DimitrisGkofas <113892241+DimitrisGkofas@users.noreply.github.com>
2024-02-13 19:36:49 +0200

    Update kernels_code.py

--- kernels_code.py
+++ kernels_code.py
@@ -3,3 +3,14 @@ kernel_one = """(float *positions, float coe) {
     positions[index] = positions[index] * coe;
 }
 """
+render = """(uchar *screen, uchar *blocks) {
+    uint xi = get_global_id(0);
+    uint yi = get_global_id(1);
+
+    uint index = xi + yi * 128;
+
+    screen[index * 3] = blocks[index];
+    screen[index * 3 + 1] = blocks[index];
+    screen[index * 3 + 2] = blocks[index];
+}
+"""
