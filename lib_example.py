66e81b5e442fb1d12e88ddefb3e367b4e80b6b33
DimitrisGkofas <113892241+DimitrisGkofas@users.noreply.github.com>
2024-02-28 14:21:38 +0200

    Update lib_example.py

--- lib_example.py
+++ lib_example.py
@@ -1,10 +1,10 @@
 from KERNEL import programs_ios_class
-from kernels_code import kernel_one
+from kernels_code import func
 import numpy as np
 '''
 gpu_programs = programs_ios_class()
 table_pos = gpu_programs.new_table('pos', (8,), np.float32)
-gpu_programs.new_program('progr_1', kernel_one, (8,))
+gpu_programs.new_program('progr_1', func, (8,))
 
 gpu_programs.table('pos')[0] = np.float32(8)
 gpu_programs.table('pos')[1] = np.float32(1)
