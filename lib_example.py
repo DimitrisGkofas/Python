7ac2182c70511ad03936be59bff6964d8e008d26
DimitrisGkofas <113892241+DimitrisGkofas@users.noreply.github.com>
2024-02-28 14:25:43 +0200

    Update lib_example.py

--- lib_example.py
+++ lib_example.py
@@ -2,12 +2,12 @@ from KERNEL import programs_ios_class
 from kernels_code import func
 import numpy as np
 '''
-gpu_programs = programs_ios_class()
-table_pos = gpu_programs.new_table('pos', (8,), np.float32)
-gpu_programs.new_program('progr_1', func, (8,))
+gpu_programs = programs_ios_class() # make an instance of the main class
+table_pos = gpu_programs.new_table('pos', table_input = np.zeros(shape = (8), dtype = np.float32) # make an input 1dimentional table with 8cells
+gpu_programs.new_program('progr_1', func, (8,)) # make the program based on the input string function with 8 compute units on one axes the axes X or arange 8 cus in 1d
 
-gpu_programs.table('pos')[0] = np.float32(8)
-gpu_programs.table('pos')[1] = np.float32(1)
+gpu_programs.table('pos')[0] = np.float32(8) # set some values as like you do in numpy
+gpu_programs.table('pos')[1] = np.float32(1) # set some values as like you do in numpy
 gpu_programs.run_program('progr_1', 'pos', np.float32(1.5))
 print(gpu_programs.table('pos'))
 '''
